3ab7c39ea9ed29a6149b4cdfdc2e2991bbc5570b
Radha Krishna <radha_krishna@epam.com>
2024-10-03 00:45:32 +0530

    Update the source code using model mapper in the productServiceImpl class

--- src/main/java/epam/com/example/Second_Spring_Boot_Application/SecondSpringBootApplication.java
+++ src/main/java/epam/com/example/Second_Spring_Boot_Application/SecondSpringBootApplication.java
@@ -1,11 +1,17 @@
 package epam.com.example.Second_Spring_Boot_Application;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SecondSpringBootApplication {
 
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SecondSpringBootApplication.class, args);
 	}
--- src/main/java/epam/com/example/Second_Spring_Boot_Application/service/ProductServiceImpl.java
+++ src/main/java/epam/com/example/Second_Spring_Boot_Application/service/ProductServiceImpl.java
@@ -4,6 +4,9 @@ import epam.com.example.Second_Spring_Boot_Application.Mapper.ProductMapper;
 import epam.com.example.Second_Spring_Boot_Application.dto.ProductDto;
 import epam.com.example.Second_Spring_Boot_Application.model.Product;
 import epam.com.example.Second_Spring_Boot_Application.repositories.ProductRepo;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,23 +15,30 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+@AllArgsConstructor
+@NoArgsConstructor
+
 @Service
 public class ProductServiceImpl implements ProductService {
 
     @Autowired
     private ProductRepo productRepo;
 
+    @Autowired
+    private ModelMapper modelMapper;
+
     @Override
     public ProductDto saveProduct(ProductDto productDto) {
 
         // Convert this ProductDto to JPA entity to save the product in DataBase.
 
-        Product product  = ProductMapper.mapToProduct(productDto);
+       // Product product  = ProductMapper.mapToProduct(productDto);
+        Product product = modelMapper.map(productDto,Product.class);
         Product savedProduct =  productRepo.save(product);
 
 // COnvert JPA entity into ProductDto
-        ProductDto savedProductDto = ProductMapper.mapToDto(savedProduct);
-
+      //  ProductDto savedProductDto = ProductMapper.mapToDto(savedProduct);
+        ProductDto savedProductDto = modelMapper.map(savedProduct,ProductDto.class);
         return savedProductDto;
     }
 
@@ -36,14 +46,16 @@ public class ProductServiceImpl implements ProductService {
         List<Product> list = productRepo.findAll();
 
         List<ProductDto> listDto = new ArrayList<ProductDto>();
-        listDto =  list.stream().map(ProductMapper :: mapToDto).collect(Collectors.toList());
+       // listDto =  list.stream().map(ProductMapper :: mapToDto).collect(Collectors.toList());
+        listDto = list.stream().map((products) -> modelMapper.map(products,ProductDto.class)).collect(Collectors.toList());
         return listDto;
     }
 
     public ProductDto getById(Integer id){
         Optional<Product> optionalProduct =  productRepo.findById(id);
         Product product = optionalProduct.get();
-        return ProductMapper.mapToDto(product);
+       // return ProductMapper.mapToDto(product);
+        return modelMapper.map(product,ProductDto.class);
     }
 
     public ProductDto updateProduct(ProductDto product){
@@ -58,7 +70,8 @@ public class ProductServiceImpl implements ProductService {
 
         Product updatedProduct = productRepo.save(exstingProduct);
 
-        return ProductMapper.mapToDto(updatedProduct);
+       // return ProductMapper.mapToDto(updatedProduct);
+        return modelMapper.map(updatedProduct,ProductDto.class);
     }
 
     public void deleteProduct(Integer id){
